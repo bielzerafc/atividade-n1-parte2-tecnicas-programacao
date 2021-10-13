ea1baeea4deed401b0dfe9aa9f26197c536453ff
joão yonéa <89917865+sorieguenight@users.noreply.github.com>
2021-10-13 10:26:54 -0300

    Update n1-parte2.py
    Estou adicionando as funções responsáveis pelas opções 3, 4 e 5.

--- n1-parte2.py
+++ n1-parte2.py
@@ -28,3 +28,31 @@ def mostrarlistaalfa():   #função para a opção 2.2
     for listaemalfa in listaalfa:
         i = i + 1
         print(i ," - ", listaemalfa)
+        
+def buscarusuario(nome):   #função para a opção 3
+    for usuario in lista:
+        if nome == usuario["nome"]:
+            print("Nome: ", usuario["nome"], " E-mail: " , usuario["e-mail"])
+
+def deletarusuario(email):   #função para a opção 4
+    for dados in lista:
+        if email == dados["e-mail"]:
+            lista.remove(dados)
+    for dados in dadoscadastro:
+        quebra = dados.split(" ")
+        if email == quebra[3]: 
+            dadoscadastro.remove(dados)
+
+def alterarnome(email):   #função para a opção 5
+    novonome = input("Insira o novo nome do Usuario: ")
+    for dados in lista:
+        if email == dados["e-mail"]:
+            dados["nome"] = novonome
+    for dados in dadoscadastro:
+        quebra = dados.split(" ")
+        if email == quebra[3]:  
+            dadoscadastro.remove(dados)
+            novousuario = quebra
+            novousuario[1] = novonome
+            novousuario = novousuario[0] + " " + novousuario[1] + " " + novousuario[2] + " " + novousuario[3]
+            dadoscadastro.append(novousuario)
